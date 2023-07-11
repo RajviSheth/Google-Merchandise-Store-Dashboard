4a0a233a09db30381573a98867081bbca75e9863
RajviSheth <rajvi.sheth@gmail.com>
2023-07-11 15:40:17 -0400

    Initial commit

--- queries.sql
+++ queries.sql
@@ -10,19 +10,23 @@ _TABLE_SUFFIX BETWEEN '20170501' AND '20170630'
 GROUP BY
 device.operatingSystem
 ORDER BY
-total_transactions DESC
+total_transactions DESC;
 
 -- 2.	HOW POPULAR IS THE ANDROID OPERATING
 -- SYSTEM WITH YOUR USERS (WHERE DEVICE.OPERATINGSYSTEM = 'ANDROID')?
+-- Ans. Total 13% of our users use Android Operating System
+
 
 -- 3.	ARE MOST OF YOUR USERS ACCESSING THE SITE FROM A MOBILE DEVICE?
+-- Ans. Most of the users are not accessing the site via a mobile device!
+
 
 -- 4.	THE REAL BOUNCE RATE IS DEFINED AS THE PERCENTAGE OF VISITS WITH A SINGLE
 -- PAGEVIEW. FOR TRAFFIC SOURCES WHERE WE HAD MORE THAN 100 VISITS, WHAT
 -- WAS THE REAL BOUNCE RATE PER TRAFFIC SOURCE?
 
-SELECT
-  trafficSource.source AS source,
+SELECT 
+trafficSource.source AS source,
   COUNT(*) AS total_visits,
   COUNTIF(totals.hits = 1) AS single_page_visits,
   ROUND(COUNTIF(totals.hits = 1) / COUNT(*) * 100, 2) AS bounce_rate
@@ -40,6 +44,13 @@ ORDER BY
 
 -- 5.	GIVEN THAT THIS IS GOOGLE'S STORE, WHAT IDEAS DO YOU THINK OUR MARKETERS
 -- SHOULD TAKE AWAY FROM THIS?
+-- Ans.  - Demonstrate the importance of data-driven decision making
+-- - Encourage exploration and experimentation
+-- - Google's merchandise store dataset, can help businesses gain a deep
+-- - understanding of their website and customer data
+
+
+
 -- 6.	FOR USERS WHO CAME TO US FROM YOUTUBE.COM (WHERE TRAFFICSOURCE.SOURCE
 -- = 'YOUTUBE.COM'), WHAT WAS THE AVERAGE NUMBER OF PRODUCT PAGEVIEWS FOR
 -- USERS WHO MADE A PURCHASE IN MAY AND JUNE OF 2017?
